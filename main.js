65f171fe689b13d580116a5270e2c8bfb3cb7326
Rob Woolley <rob.woolley@windriver.com>
2017-02-01 16:04:27 -0500

    Add IBM Bluemix starter app
    This start app can report analog and digital input to IBM Bluemix.
    
    By default, it sends the telemetry to the IBM Watson IoT Platform
    QuickStart page.  If you register your device with the Internet of
    Things Starter from the Catalog you may also send the telemetry to a
    private MQTT broker.
    
    Signed-off-by: Rob Woolley <rob.woolley@windriver.com>

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 /*
- * Blank IoT Node.js starter app.
  *
- * Use this template to start an IoT Node.js app on any supported IoT board.
- * The target board must support Node.js. It is helpful if the board includes
- * support for I/O access via the MRAA and UPM libraries.
+ * Copyright (c) 2017 Wind River Systems, Inc.
  *
- * https://software.intel.com/en-us/xdk/docs/lp-xdk-iot
  */
 
 
@@ -16,11 +12,81 @@
 
 "use strict" ;
 
-
+// Javascript docs: https://iotdk.intel.com/docs/master/mraa/node/modules/mraa.html
 var mraa = require("mraa") ;
 
-// add any UPM requires that you need
-// and the rest of your app goes here
-// see the samples for more detailed examples
+// If missing, run: npm install mqtt --save
+var mqtt = require("mqtt") ;
 
-console.log(mraa) ;     // prints mraa object to XDK IoT debug output panel
+// Change to your Edison's MAC Address
+var MAC = "784b87aaea97" 
+
+// Set your organization when using a registered device.
+var ORG = 'quickstart';
+var TYPE = 'iotquick-edison';
+var ID = MAC;
+
+// Set your authentication when using a registered device.
+var AUTHTOKEN = '0123456789abcdefg';
+
+var PROTOCOL = 'mqtt';
+var BROKER = ORG + '.messaging.internetofthings.ibmcloud.com';
+var PORT = 1883;
+var DEBUG = 0;
+
+//Create the url string
+var URL = PROTOCOL + '://' + BROKER;
+URL += ':' + PORT; 
+// Default URL is 'mqtt://quickstart.messaging.internetofthings.ibmcloud.com:1883'
+
+var CLIENTID= 'd:' + ORG;
+CLIENTID += ':' + TYPE;
+CLIENTID += ':' + ID;
+
+var AUTHMETHOD = 'use-token-auth';
+var TOPIC = 'iot-2/evt/status/fmt/json';
+
+if (ORG == "quickstart") {
+    var client  = mqtt.connect(URL, { clientId: CLIENTID });
+    console.log("Go to https://quickstart.internetofthings.ibmcloud.com/#/device/"+ID+"/sensor/");   
+} else {
+    var client  = mqtt.connect(URL, { clientId: CLIENTID, username: AUTHMETHOD, password: AUTHTOKEN });
+}
+    
+client.on('connect', function () {
+  setInterval(function(){
+    var MSG = '{"d":{"Light":' + light() + ',"Button": ' + button() + ',"Sound":'+ sound() + '}}';
+    client.publish(TOPIC, MSG);//Payload is JSON
+    if (DEBUG) console.log(MSG) ;     // prints mraa object to XDK IoT debug output panel
+  }, 2000);//Keeps publishing every 2000 milliseconds.
+});
+
+// Grove Sound Sensor: http://wiki.seeed.cc/Grove-Sound_Sensor/
+// Plug the Grove Sound Sensor into A0
+var pin0 = new mraa.Aio(0);
+var sound = function() {
+    var sound_input = pin0.read();
+    
+    return parseFloat(sound_input).toFixed(4);
+}
+
+// Grove Light Sensor: http://wiki.seeed.cc/Grove-Light_Sensor/
+// Plug the Grove Sound Sensor into A1
+var pin1 = new mraa.Aio(1);
+var light = function() {
+    var light_input = pin1.read();
+    
+    return parseFloat(light_input).toFixed(4);
+}
+
+// Grove Button
+// Plug the Grove Button into D2
+// pin2.mode = mraa.MODE_PULLUP;
+var pin2 = new mraa.Gpio(2);
+pin2.dir = mraa.DIR_IN;
+
+var button = function() {
+    var button_input = pin2.read();
+    
+    return parseFloat(button_input).toFixed(4);
+}
